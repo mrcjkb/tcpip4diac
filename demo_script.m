492cc56d981c9bb17dbb6cbd10142f3e16339d1e
Marc Jakobi <jakobi@htw-berlin.de>
2017-05-26 16:56:42 +0200

    Updated demo_script with info on what the demos do

--- demo_script.m
+++ demo_script.m
@@ -1,12 +1,25 @@
-%% 1. Client in Matlab - server in FORTE.
+%% Demo 1: Client in Matlab - server in FORTE.
+%
+% What this demo does: Sends a variable to a FORTE application which
+% adds 3 to the data and sends the result back to Matlab
+
 % --> First, import the file ServerTest.sys into 4diac-IDE and compile the
 % application ServerTestApp onto FORTE, then run the following:
+var = 5;
 t = tcpip4diac('client'); % host = 'localhost'; address = 61500;
 init(t, 1) % connect to server
-out = req(t, 5); % returns 8
+out = req(t, var); % returns var + 3
+disp(out)
 init(t, 0) % disconnect
 
 %% 2. Server in Matlab - client in FORTE
+%
+% What this demo does: A FORTE application sends an integer (3) to the
+% tcpip4diac server object in Matlab. Upon receival of the data, Matlab
+% adds 3 to the variable and sends it back to FORTE. The FORTE application
+% sends the received result back to Matlab and Matlab adds 3 to it, and so
+% on...
+
 % --> Import the file ClientTest.sys into 4diac-IDE.
 % Important: The ID of the CLIENT_1 function block in the FORTE_PC.EMB_RES
 % resource must be set what is displayed in the commmand window
